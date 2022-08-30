332bef139ad7d4e5e9925524b6417581eb2c4a26
Steven Rosario <strosariogarcia@gmail.com>
2022-08-30 16:49:06 -0400

    commit tree 352764bee221f8f9be66541850bd657ca2c4f5cc

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+
+function superbowlWin(record){
+    const result = record.find(element => element.result === "W") //I did not need to make a for loop. The .find() function went through the array of objects with results of W
+    if (result !== undefined){ // To avoid the TypeError, the if statement helped when result is NOT undefined
+        return result.year;
+    }
+    
+}  
